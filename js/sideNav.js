506e5da8aae3cf4bd0c475025f1d83b587ad0af2
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2019-10-04 09:10:47 -0500

    side bar

--- js/sideNav.js
+++ js/sideNav.js
@@ -2,6 +2,6 @@ function toggleNav(x) {
     x.classList.toggle("change");
 	document.getElementById("mySidenav").style.width = 
 	window.getComputedStyle(document.getElementById("mySidenav")).width === 
-	"0px" ? "15vw" : "0px";
+	"0px" ? "160px" : "0px";
 
 }

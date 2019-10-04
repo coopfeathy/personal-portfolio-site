9774f5728245f6e3a3d5d78fd7b016c9fb731e6a
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2019-10-04 13:50:40 -0500

    update

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,7 +1,11 @@
 function toggleNav(x) {
-    x.classList.toggle("change");
-	document.getElementById("mySidenav").style.width = 
-	window.getComputedStyle(document.getElementById("mySidenav")).width === 
+	x.classList.toggle("change");
+	
+	document.getElementById("sidenav").style.width = 
+	window.getComputedStyle(document.getElementById("sidenav")).width === 
 	"0px" ? "160px" : "0px";
 
+	document.getElementById("main").style.marginLeft = 
+	window.getComputedStyle(document.getElementById("main")).marginLeft === 
+	"0px" ? "160px" : "0px";
 }
